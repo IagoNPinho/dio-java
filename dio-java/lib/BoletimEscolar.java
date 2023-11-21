ad60f7752fb8acf65b56ad27462c0bf735534256
Iago Nascimento de Pinho <iago.pinho.dev@gmail.com>
2023-11-21 19:12:55 -0300

    # Anatomia das classes 4

---
+++ dio-java/lib/BoletimEscolar.java
@@ -0,0 +1,14 @@
+public class BoletimEscolar {
+
+  //Anatomia das classes 4 - Identação
+  public static void main(String[] args) {
+    int mediaFinal = 7;
+    if (mediaFinal < 6) {
+      System.out.println("Reprovado");
+    } else if (mediaFinal == 6) {
+      System.out.println("Prova Final");
+    } else {
+      System.out.println("Aprovado");
+    }
+  }
+}
