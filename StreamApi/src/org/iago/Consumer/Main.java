d09d41b5af5b13e6687a4fc3d5c41abf398adedd
Iago <iago.pinho.dev@gmail.com>
2024-02-05 08:10:26 -0300

    # Functional Interface - Consumer<T>

---
+++ StreamApi/src/org/iago/Consumer/Main.java
@@ -0,0 +1,25 @@
+package org.iago.Consumer;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+
+        // Consumer<Integer> showPairNumbers = number -> {
+        //     if(number % 2 == 0){
+        //         System.out.println(number);
+        //     }
+        // };
+
+        // numbers.stream().forEach(showPairNumbers);
+
+        numbers.stream().forEach( number -> {
+            if ((number % 2) == 0){
+                System.out.print(number + " ");
+            }
+        });
+    }
+}
